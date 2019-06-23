bd8ab1bdb1a9aa7d7c2355637673a9d65f867362
EidtZY <52123321+EidtZY@users.noreply.github.com>
2019-06-23 19:03:10 +0800

    .

---
+++ 01.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+# Filename : 01-helloworld.py
+# author by : （Eidt)
+
+# 目的:
+# 掌握如何查看程序运行结果的方式
+
+# 该实例输出 Hello World!
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+
+；看见就好看了就考虑接口连接
+uuu
